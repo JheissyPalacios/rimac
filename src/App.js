bc245870ab17da4c487fe33c11826e92a6870370
JheissyPalacios <jheissyp@gmail.com>
2024-06-18 15:45:52 -0500

    homebase

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
     <div className="App">
       <Header/>
         <BrowserRouter>
-          <Routes basename="/rimac">
+          <Routes>
             <Route
               path="/"
               element={
