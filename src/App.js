cc6cf543261f468334d0db8c470ccaeffd82b719
JheissyPalacios <jheissyp@gmail.com>
2024-06-18 14:13:21 -0500

    delete app context

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { useState, useContext, useEffect } from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Header from "./components/Header";
-import AppContext from "./context/AppContex";
 import Login from './pages/Login';
 import './App.css';
 import Home from "./pages/Home";
@@ -10,7 +9,6 @@ function App() {
   return (
     <div className="App">
       <Header/>
-      <AppContext.Provider>
         <BrowserRouter>
           <Routes basename="/rimac">
             <Route
@@ -27,7 +25,6 @@ function App() {
             ></Route>
           </Routes>
         </BrowserRouter>
-      </ AppContext.Provider>
     </div>
   );
 }
